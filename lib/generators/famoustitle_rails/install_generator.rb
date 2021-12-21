dfd1426d0165fa411c79df6a9bd8e02b53359e44
Tha Leang <vleango@gmail.com>
2021-12-21 01:14:24 -0800

    update fameauth to use 1.2.0 (rails 7)

--- lib/generators/famoustitle_rails/install_generator.rb
+++ lib/generators/famoustitle_rails/install_generator.rb
@@ -13,7 +13,7 @@ module FamoustitleRails
         gem 'rack-cors', '~> 1.1.1'
         gem 'lockbox', '~> 0.6.5'
         gem 'blind_index', '~> 2.2.0'
-        gem 'fameauth', git: 'https://github.com/vleango/fameauth.git', tag: "1.1.0"
+        gem 'fameauth', git: 'https://github.com/vleango/fameauth.git', tag: "1.2.0"
       end
   
       def create_cors_config_file
