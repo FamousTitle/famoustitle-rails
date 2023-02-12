90e29d3e899e63d73541a83b2adf67d16d2db0a6
Tha Leang <vleango@gmail.com>
2023-02-11 22:05:51 -0600

    readd fameauth and use CORS_ORIGINS if exists

--- lib/generators/famoustitle_rails/templates/config/initializers/cors.rb
+++ lib/generators/famoustitle_rails/templates/config/initializers/cors.rb
@@ -1,7 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins *ENV.fetch('CORS_ORIGINS').split(" ").map(&:strip)
-    origins '*'
+    origins *ENV.fetch('CORS_ORIGINS', "*").split(",").map(&:strip)
 
     resource '*',
       headers: :any,
