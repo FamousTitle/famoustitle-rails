ac6c594462c74a438fd65c76507f266ae5b624db
Tha Leang <vleango@gmail.com>
2023-02-13 01:03:14 -0600

    fix HEREDOC spacing

--- lib/generators/famoustitle_rails/install_generator.rb
+++ lib/generators/famoustitle_rails/install_generator.rb
@@ -39,7 +39,7 @@ module FamoustitleRails
         
         inject_into_file file, before: "def execute" do
           <<-HEREDOC
-          include ActiveStorage::SetCurrent
+include ActiveStorage::SetCurrent
 
           HEREDOC
         end
@@ -49,21 +49,21 @@ module FamoustitleRails
         file = 'app/graphql/types/mutation_type.rb'
         inject_into_file file, after: "MutationType < Types::BaseObject" do
           <<-HEREDOC
+    
+    field :user_reset_password, String, null: false do
+      argument :password_reset_token, String, required: true
+      argument :password, String, required: true
+      argument :password_confirmation, String, required: true
+    end
 
-            field :user_reset_password, String, null: false do
-              argument :password_reset_token, String, required: true
-              argument :password, String, required: true
-              argument :password_confirmation, String, required: true
-            end
-        
-            def user_reset_password(password_reset_token:, password:, password_confirmation:)
-              User.reset_password_by_token(
-                reset_password_token: reset_password_token,
-                password: password,
-                password_confirmation: password_confirmation
-              )
-              "ok"
-            end
+    def user_reset_password(password_reset_token:, password:, password_confirmation:)
+      User.reset_password_by_token(
+        reset_password_token: reset_password_token,
+        password: password,
+        password_confirmation: password_confirmation
+      )
+      "ok"
+    end
 
           HEREDOC
         end
@@ -71,18 +71,18 @@ module FamoustitleRails
 
       def add_graphql_queries
         file = 'app/graphql/types/query_type.rb'
-        inject_into_file file, after: "include GraphQL::Types::Relay::HasNodesField" do
+        inject_into_file file, after: "# They will be entry points for queries on your schema." do
           <<-HEREDOC
+    
+    field :password_reset_token, String, null: false do
+      argument :email, String, required: true
+    end
 
-              field :password_reset_token, String, null: false do
-                argument :email, String, required: true
-              end
-          
-              def password_reset_token(email:)
-                user = User.find_by(email: email)
-                user.send_password_reset_email if user.present?
-                "ok"
-              end
+    def password_reset_token(email:)
+      user = User.find_by(email: email)
+      user.send_password_reset_email if user.present?
+      "ok"
+    end
 
           HEREDOC
         end
@@ -94,9 +94,8 @@ module FamoustitleRails
         inject_into_file file, after: '< GraphQL::Schema' do
           <<-HEREDOC
           
-          disable_schema_introspection_entry_point unless Rails.env.development?
-          disable_type_introspection_entry_point unless Rails.env.development?
-
+  disable_schema_introspection_entry_point unless Rails.env.development?
+  disable_type_introspection_entry_point unless Rails.env.development?
           HEREDOC
         end
       end
